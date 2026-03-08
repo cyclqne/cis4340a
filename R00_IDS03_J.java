55b938313a8127da0aaf9f88a3b5297de5bfaa94
Colin Briggs <114253074+cyclqne@users.noreply.github.com>
2026-03-08 00:48:25 -0500

    Update R00_IDS03_J.java
    Add noncompliant version of R00_IDS03_J

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,1 +1,16 @@
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
+public class R00_IDS03_J {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
+        String username = "user<script>";
+        boolean loginSuccessful = false;
+
+        if (loginSuccessful) {
+            logger.severe("User login succeeded for: " + username);
+        } else {
+            logger.severe("User login failed for: " + username);
+        }
+    }
+}
