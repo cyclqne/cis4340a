75a058ebc5c68e2863c221e9b0c3e162cc98a300
Colin Briggs <114253074+cyclqne@users.noreply.github.com>
2026-03-08 00:53:32 -0500

    Update R06_MET01_J.java
    Add noncompliant version of R06_MET01_J

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,1 +1,15 @@
 
+public class R06_MET01_J {
+    public static void main(String[] args) {
+        System.out.println(getAbsAdd(5, 10));
+    }
+
+    public static int getAbsAdd(int x, int y) {
+        assert x != Integer.MIN_VALUE;
+        assert y != Integer.MIN_VALUE;
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
+        assert (absX <= Integer.MAX_VALUE - absY);
+        return absX + absY;
+    }
+}
