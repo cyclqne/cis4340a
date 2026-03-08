e6bcdc906752aff7831a5c660e292512af03b4dd
Colin Briggs <114253074+cyclqne@users.noreply.github.com>
2026-03-08 00:51:22 -0500

    Update R02_XP00_J.java
    Add noncompliant version of R02_XP00_J

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,1 +1,12 @@
+import java.io.File;
 
+public class R02_XP00_J {
+    public static void main(String[] args) {
+        deleteFile();
+    }
+
+    public static void deleteFile() {
+        File someFile = new File("someFileName.txt");
+        someFile.delete();
+    }
+}
