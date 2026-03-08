783e8ba952498604396b7de68c034b6f353c2a41
Colin Briggs <114253074+cyclqne@users.noreply.github.com>
2026-03-08 00:52:12 -0500

    Update R03_NUM03_J.java
    Add noncompliant version of R03_NUM03_J

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,1 +1,17 @@
+import java.io.DataInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
+public class R03_NUM03_J {
+    public static void main(String[] args) throws IOException {
+        byte[] data = {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+        DataInputStream is = new DataInputStream(new ByteArrayInputStream(data));
+
+        int value = getInteger(is);
+        System.out.println(value);
+    }
+
+    public static int getInteger(DataInputStream is) throws IOException {
+        return is.readInt();
+    }
+}
